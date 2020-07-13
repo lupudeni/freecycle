b953572c950489649d06a93a40030bd4d6865ffd
lupudeni <denisa.s.lupu@gmail.com>
2020-07-13 09:08:43 +0300

    First Commit

---
+++ src/main/java/com/denisalupu/freecycle/FreeCycleApplication.java
@@ -0,0 +1,13 @@
+package com.denisalupu.freecycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeCycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeCycleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/denisalupu/freecycle/FreeCycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denisalupu.freecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeCycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
